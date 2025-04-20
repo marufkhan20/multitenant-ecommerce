f254945bdf13f777c88e924f2f55da00c8e5e5df
Rashedul Islam Maruf <mdm326040@gmail.com>
2025-04-20 18:01:30 +0600

    Design layout

---
+++ src/app/(home)/footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="flex border-t justify-between font-medium p-6">
+      <div className="flex gap-2 items-center">
+        <p>funroad, Inc.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
