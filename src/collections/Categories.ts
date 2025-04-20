abe9f6b339a70607b67945f3de981175d8e53319
Rashedul Islam Maruf <mdm326040@gmail.com>
2025-04-20 22:36:55 +0600

    Payload integration

---
+++ src/collections/Categories.ts
@@ -0,0 +1,22 @@
+import type { CollectionConfig } from "payload";
+
+export const Categories: CollectionConfig = {
+  slug: "categories",
+  fields: [
+    {
+      name: "name",
+      type: "text",
+      required: true,
+      unique: true,
+    },
+    {
+      name: "description",
+      type: "textarea",
+    },
+    {
+      name: "image",
+      type: "upload",
+      relationTo: "media",
+    },
+  ],
+};
